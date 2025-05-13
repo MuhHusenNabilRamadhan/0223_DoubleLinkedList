d39fc2cf9dfeb6efae28c4a41b1172efcb9d3021
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-05-13 10:09:37 +0700

    memberikan nilai pada nomhs dan nama

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -22,4 +22,8 @@ void addNode()
 
     //pembuatan node dan pemberian value untuk data noMhs dan nama
     Node *newNode = new Node(); //step 1 buat node baru
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs; //assign value to the data field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name; //assign value the data field of th
 }
